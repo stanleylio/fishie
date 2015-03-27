61d55a620d9568a9725c801d1fd8edb056f76f3b
Stanley Lio <hlio@usc.edu>
2015-03-27 19:00:41 +0000

    automation sh stuff

--- www/sendWWWtoWeb2.sh
+++ www/sendWWWtoWeb2.sh
@@ -3,4 +3,4 @@
 # CSV export is slow, so I isolated the website upload part so that it can be scheduled more frequently.
 # --SL
 
-scp -r ./* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest/
+scp -r $HOME/base/www/* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest/
