fe705cfa97a593dc247c9cda12b41bc58e7c0a83
Stanley Lio <hlio@usc.edu>
2015-03-27 19:07:02 +0000

    automation stuff still

--- dispatch_logger.sh
+++ dispatch_logger.sh
@@ -12,7 +12,7 @@ if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= | grep -P "^\s*$(cat ${PIDFILE
 fi
 
 #/path/to/myprogram > $HOME/tmp/myprogram.log &
-cd /root/base && /usr/bin/python /root/base/sampling.py &
+cd /root/node && /usr/bin/python /root/node/sampling.py &
 
 echo $! > "${PIDFILE}"
 chmod 644 "${PIDFILE}"
