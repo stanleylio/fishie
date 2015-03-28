e5749df15170895dc8004025bdbf8428f802779f
Stanley Lio <hlio@usc.edu>
2015-03-28 00:34:33 +0000

    upload.sh

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# CSV export is slow, so I isolated the website upload part so that page upload can be scheduled more frequently.
+# --SL
+
+#scp -r ./* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest/
+
+echo "upload webpage"
+rsync -avzhe ssh --progress ./www/* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest
+
+echo "upload database"
+rsync -avzhe ssh --progress ./storage/sensor_data.db glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest
--- www/sendWWWtoWeb2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# CSV export is slow, so I isolated the website upload part so that it can be scheduled more frequently.
-# --SL
-
-cd $HOME/node/www
-scp -r ./* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest/
