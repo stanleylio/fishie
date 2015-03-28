588e9e7f701aa620c2363228eefed28f3f398b8d
Stanley Lio <hlio@usc.edu>
2015-03-28 00:44:09 +0000

    minor improvement

--- upload.sh
+++ upload.sh
@@ -3,8 +3,6 @@
 # CSV export is slow, so I isolated the website upload part so that page upload can be scheduled more frequently.
 # --SL
 
-#scp -r ./* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest/
-
 echo "upload webpage"
 rsync -avzhe ssh --progress ./www/* glazer@imina.soest.hawaii.edu:/export/imina2/httpd/htdocs/oceanography/glazer/Brian_T._Glazer/research/DataLoggers/benchtest
 
