9423f64e9c691ee93f0cb6d8131bab62ce38822b
root <root@node_019.(none)>
2015-09-29 04:27:19 +0000

    .

--- drivers/indicators.py
+++ drivers/indicators.py
@@ -3,19 +3,26 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 
 red_pin = 'P8_8'
+yellow_pin = 'P8_10'
 green_pin = 'P8_9'
-
-def green_on():
-    GPIO.output(green_pin,GPIO.HIGH)
-
-def green_off():
-    GPIO.output(green_pin,GPIO.LOW)
 
 def red_on():
     GPIO.output(red_pin,GPIO.HIGH)
 
 def red_off():
     GPIO.output(red_pin,GPIO.LOW)
+
+def yellow_on():
+    GPIO.output(yellow_pin,GPIO.HIGH)
+
+def yellow_off():
+    GPIO.output(yellow_pin,GPIO.LOW)
+
+def green_on():
+    GPIO.output(green_pin,GPIO.HIGH)
+
+def green_off():
+    GPIO.output(green_pin,GPIO.LOW)
 
 def usrx_on(x):
     assert x in [0,1,2,3]
@@ -29,6 +36,7 @@ def usrx_off(x):
 
 def indicators_setup():
     GPIO.setup(red_pin,GPIO.OUT)
+    GPIO.setup(yellow_pin,GPIO.OUT)
     GPIO.setup(green_pin,GPIO.OUT)
     with open('/sys/class/leds/beaglebone:green:usr0/trigger','w') as f:
         f.write('none')
@@ -39,6 +47,7 @@ def indicators_setup():
     with open('/sys/class/leds/beaglebone:green:usr3/trigger','w') as f:
         f.write('none')
     red_off()
+    yellow_off()
     green_off()
     usr0_off()
     usr1_off()
@@ -67,15 +76,38 @@ if '__main__' == __name__:
         while True:
             print('red')
             red_on()
+            yellow_off()
             green_off()
+            usr0_off()
+            usr1_off()
+            usr2_off()
+            usr3_off()
+            time.sleep(t)
+
+            print('yellow')
+            red_off()
+            yellow_on()
+            green_off()
+            usr0_off()
+            usr1_off()
+            usr2_off()
+            usr3_off()
             time.sleep(t)
 
             print('green')
             red_off()
+            yellow_off()
             green_on()
+            usr0_off()
+            usr1_off()
+            usr2_off()
+            usr3_off()
             time.sleep(t)
 
             print('usr0')
+            red_off()
+            yellow_off()
+            green_off()
             usr0_on()
             usr1_off()
             usr2_off()
@@ -83,6 +115,9 @@ if '__main__' == __name__:
             time.sleep(t)
 
             print('usr1')
+            red_off()
+            yellow_off()
+            green_off()
             usr0_off()
             usr1_on()
             usr2_off()
@@ -90,6 +125,9 @@ if '__main__' == __name__:
             time.sleep(t)
 
             print('usr2')
+            red_off()
+            yellow_off()
+            green_off()
             usr0_off()
             usr1_off()
             usr2_on()
@@ -97,17 +135,15 @@ if '__main__' == __name__:
             time.sleep(t)
 
             print('usr3')
+            red_off()
+            yellow_off()
+            green_off()
             usr0_off()
             usr1_off()
             usr2_off()
             usr3_on()
             time.sleep(t)
-
-            usr0_off()
-            usr1_off()
-            usr2_off()
-            usr3_off()
-            
+           
     except KeyboardInterrupt:
         print 'user interrupted'
 
