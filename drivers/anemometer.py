2b616f8974d308328c192d6de6d3c359fd3e108a
root <root@(none).(none)>
2015-10-02 06:07:03 +0000

    .

--- drivers/anemometer.py
+++ drivers/anemometer.py
@@ -5,6 +5,7 @@
 import sys,time,os
 sys.path.append(os.path.join(os.path.dirname(__file__),'Adafruit_GPIO'))
 from I2C import Device
+from scipy.signal import medfilt
 
 class Anemometer(object):
     def __init__(self,addr=0x50):
@@ -16,14 +17,20 @@ class Anemometer(object):
         
         S = []
         for i in range(100):
+            # These kinds of low-level calls never "fail" - only the
+            # application tell whether the return values make sense
+            # or not.
             v = self._i2c.readU16(speed_reg)
             S.append(v)
             time.sleep(0.001)
 
         if len(S) > 0:
+            # A cheap way to get rid of the failed values is the
+            # median filter.
+            S = medfilt(S,7)
             v = sum(S)/len(S)
             v = self.conv(v)
-            
+
             # What does "negative wind speed" even mean?
             # happens when the anemometer lose power. "0m/s" is at 0.4V.
             v = max([v,0])
@@ -48,7 +55,7 @@ if '__main__' == __name__:
     
     while True:
         r = a.read()
-        print("\x1b[2J\x1b[;H")
+        #print("\x1b[2J\x1b[;H")
         #print '{:.2f} m/s'.format(a.read())
         print '{},{:.2f},{:.2f}'.format(time.time(),r['speed'],r['gust'])
         #time.sleep(1)
