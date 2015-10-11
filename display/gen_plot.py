762b1c7fc792e99f377d1a5323ec1976998272e3
root <root@node-019.localdomain>
2015-10-11 07:39:56 +0000

    .

--- display/gen_plot.py
+++ display/gen_plot.py
@@ -94,6 +94,7 @@ if '__main__' == __name__:
     assert time_col is not None
 
     for node_id in IDs:
+        PRINT('- - - - -')
         node_tag = 'node-{:03d}'.format(node_id)
         config_file = '../config/{}.ini'.format(node_tag)
 
