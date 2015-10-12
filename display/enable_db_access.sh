0c911d6b885a2954467bdd5e645742779ec87ea6
Stanley Hou In Lio <stanleylio@gmail.com>
2015-10-12 00:28:52 +0000

    grant write access to database to all so that apache can write to the temporary database files

--- display/enable_db_access.sh
+++ display/enable_db_access.sh
@@ -7,6 +7,17 @@
 
 cd ~/node/storage
 
+# this works:
+chmod a+w sensor_data.db
+chmod a+w sensor_data.db-shm
+chmod a+w sensor_data.db-wal
+
+exit 0
+
+# this also works, but only for as long as the -shm and -wal
+# persist once they are deleted and recreated by sampling.py
+# again, their group ownership return back to that of the user
+# executing sampling.py
 sudo chgrp www-data sensor_data.db
 sudo chgrp www-data sensor_data.db-shm
 sudo chgrp www-data sensor_data.db-wal
@@ -15,8 +26,7 @@ sudo chmod g+w sensor_data.db
 sudo chmod g+w sensor_data.db-shm
 sudo chmod g+w sensor_data.db-wal
 
-cd ..
-sudo chgrp www-data storage/
-sudo chmod g+w storage/
+sudo chgrp www-data .
+sudo chmod g+w .
 
 cd
