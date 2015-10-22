5b4637821639b12128e786d87a89914b6374ad2c
Stanley Lio <stanleylio@gmail.com>
2015-10-22 07:40:02 +0000

    .

--- www/index.php
+++ www/index.php
@@ -3,12 +3,14 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="refresh" content="60">
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 		<link href="jquery-ui/jquery-ui.min.css" rel="stylesheet" type="text/css">
 		<script src="jquery/jquery.min.js" type="text/javascript"></script>
 		<script src="jquery.timeago.js" type="text/javascript"></script>
 		
 		<link href="node_page.css" rel="stylesheet" type="text/css">
+		<title>PoH</title>
 	</head>
 	<body>
 		<div class="container-fluid">
