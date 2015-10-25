ca6782f15320ca302cc88a81dcc646513d95e37c
Stanley Lio <stanleylio@gmail.com>
2015-10-25 05:30:30 +0000

    .

---
+++ drivers/watchdog.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#
+# Stanley Hou In Lio, hlio@hawaii.edu
+# October 1, 2015
+import sys,time,os
+sys.path.append(os.path.join(os.path.dirname(__file__),'Adafruit_GPIO'))
+from I2C import Device
+from scipy.signal import medfilt
+
+
+class Watchdog(object):
+    def __init__(self,addr=0x51,bus=1):
+        self._i2c = Device(addr,busnum=bus)
+
+    def reset(self):
+        tmp = self._i2c.readU16(10)
+        return None
+
+
+if '__main__' == __name__:
+
+    w = Watchdog(bus=2)
+    w.reset()
+
