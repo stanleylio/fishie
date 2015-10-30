30ad41deed595325a11836597af72b8e0ba88a3a
Stanley Lio <stanleylio@gmail.com>
2015-10-30 00:57:36 +0000

    .

--- setup/backup_settings.sh
+++ setup/backup_settings.sh
@@ -7,6 +7,8 @@ cat /etc/hostname > hostname.txt
 cat /etc/hosts > hosts.txt
 cat /etc/resolv.conf > resolv.conf.txt
 cat /etc/rc.local > rc.local.txt
+
 cat /etc/supervisor/supervisord.conf > supervisord.conf
+cat /etc/supervisor/conf.d/sampling.conf > sampling.conf
 
 
