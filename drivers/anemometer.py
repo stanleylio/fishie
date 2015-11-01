c65fe43d7053d39992fd86270d92c07a0b0e0c90
Stanley Lio <stanleylio@gmail.com>
2015-11-01 04:06:31 +0000

    .

--- drivers/anemometer.py
+++ drivers/anemometer.py
@@ -27,22 +27,25 @@ class Anemometer(object):
     # average wind speed (8-bit resolution due to LUFA RingBuffer limit)
     # this takes one minute to stabilize
     def average(self):
-        v = self.conv(self._i2c.readU8(self.speed_reg))
-        v = round(v*10)/10.
-        return v
+        #v = self.conv(self._i2c.readU8(self.speed_reg))
+        v = self.conv(self._i2c.readU16(self.speed_reg))
+        
+        return max(round(v*10)/10.,0)
 
     # wind gust (8-bit resolution due to LUFA RingBuffer limit)
     def gust(self):
-        v = self.conv(self._i2c.readU8(self.gust_reg))
-        v = round(v*10)/10.
-        return v
+        #v = self.conv(self._i2c.readU8(self.gust_reg))
+        v = self.conv(self._i2c.readU16(self.gust_reg))
+        return round(v*10)/10.
+
+    #def duh(self):
+    #    return self._i2c.readU16(self.speed_reg)
 
     @staticmethod
     def conv(v):
-        #s = v/1023.0*2.56;     # LUFA RingBuffer doesn't take uint16_t
-        s = v/255.0*2.56;
-        s = 32.4/1.6*(s - 0.4);
-        return s
+        #s = v/255.0*2.56;     # LUFA RingBuffer doesn't take uint16_t
+        s = v/1023.0*2.56;
+        return 32.4/1.6*(s - 0.4);
 
 
 if '__main__' == __name__:
@@ -51,9 +54,11 @@ if '__main__' == __name__:
 
     while True:
         r = a.read()
-        #print('\x1b[2J\x1b[;H')
-        print('Raw ADC reg={}\t\tAverage={:.1f}m/s\t\tGust={:.1f}m/s\t'.format(r['raw'],r['speed'],r['gust']))
+        print('\x1b[2J\x1b[;H')
+        print('Raw ADC reg={}\t\tAverage={:.1f}m/s\t\tGust={:.1f}m/s\t'.\
+              format(r['raw'],r['speed'],r['gust']))
         #print a.average()
         #print a.raw()
-        time.sleep(1)
+        #print a.duh()
+        time.sleep(0.5)
 
--- last_heard_from.py
+++ last_heard_from.py
@@ -12,8 +12,8 @@ from storage.storage import storage_read_only
 
 dbfile = './storage/sensor_data.db'
 if not exists(dbfile):
-    #dbfile = '/home/nuc/data/base-003/storage/sensor_data.db'
-    dbfile = '/home/nuc/data/base-004/storage/sensor_data.db'
+    dbfile = '../data/base-003/storage/sensor_data.db'
+    #dbfile = '../data/base-004/storage/sensor_data.db'
 
 store = storage_read_only(dbfile=dbfile)
 
@@ -23,22 +23,21 @@ nodes = [int(t[5:8]) for t in store.get_list_of_tables() if re.match('node_\d{3}
 while True:
     print('\x1b[2J\x1b[;H')
     
-    print(dbfile)
-    #print('Last heard from (days hh:mm:ss)...')
-    print('Last heard from (hh:mm:ss)...')
+    print('From {}'.format(dbfile))
+    print('Last heard from (days hh:mm:ss)...')
+    #print('Last heard from (hh:mm:ss)...')
     for node_id in nodes:
         try:
             t = store.read_last_N(node_id,'ReceptionTime')['ReceptionTime'][0]
             ago = (datetime.now() - t).total_seconds()
-            #print 'node {}:\t{:.0f}d {:2.0f}:{:02.0f}:{:02.0f} ago'.\
-            #      format(node_id,ago//(3600*24),(ago%(3600*24))//3600,(ago%3600)//60,ago%60)
-            print 'node {}:\t{:2.0f}:{:02.0f}:{:02.0f} ago'.\
-                  format(node_id,(ago%(3600*24))//3600,(ago%3600)//60,ago%60)
+            print 'node {}:\t{:.0f}d {:2.0f}:{:02.0f}:{:02.0f} ago'.\
+                  format(node_id,ago//(3600*24),(ago%(3600*24))//3600,(ago%3600)//60,ago%60)
+            #print 'node {}:\t{:2.0f}:{:02.0f}:{:02.0f} ago'.\
+            #      format(node_id,(ago%(3600*24))//3600,(ago%3600)//60,ago%60)
         except TypeError:
             pass
         except:
             traceback.print_exc()
 
     time.sleep(1)
-
 
