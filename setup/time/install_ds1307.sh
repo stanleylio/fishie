9d5c3eef75922a771a7fc2d7fac6fe4873c70e9a
Stanley Lio <stanleylio@gmail.com>
2015-12-07 22:46:53 +0000

    .

--- setup/time/install_ds1307.sh
+++ setup/time/install_ds1307.sh
@@ -35,7 +35,10 @@ hwclock --show --rtc=/dev/rtc1
 echo "Installing service..."
 # whichever works... oh man.
 cp /root/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307.service
+cp /root/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307_rpi2.service
 systemctl enable rtc-ds1307.service
 systemctl start rtc-ds1307.service
+systemctl enable rtc-ds1307_rpi2.service
+systemctl start rtc-ds1307_rpi2.service
 
 # shutdown -r now
