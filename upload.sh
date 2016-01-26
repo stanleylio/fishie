439d9e0e5c1fca8d9fa12f9c7e86dcda055bc423
Stanley Lio <stanleylio@gmail.com>
2016-01-26 03:30:18 +0000

    stopped uploading thte backup copies of database

--- upload.sh
+++ upload.sh
@@ -8,6 +8,8 @@
 
 # the parent directory must exist on the server first as rsync won't recreate the entire dir structure
 
+date
+
 # glazerlab-i7nuc
 BENCHTEST_DIR="nuc@128.171.153.115:/home/nuc/data/$(hostname)"
 
@@ -16,8 +18,9 @@ BENCHTEST_DIR="nuc@128.171.153.115:/home/nuc/data/$(hostname)"
 REMOTE_RSYNC_PATH=/usr/bin/rsync
 
 echo "upload database"
-rsync -avzhe ssh --rsync-path=$REMOTE_RSYNC_PATH --progress ./storage/sensor_data* $BENCHTEST_DIR/storage/
+#rsync -avzhe ssh --rsync-path=$REMOTE_RSYNC_PATH --progress ./storage/sensor_data* $BENCHTEST_DIR/storage/
+rsync -avzhe ssh --rsync-path=$REMOTE_RSYNC_PATH --progress ./storage/sensor_data.db* $BENCHTEST_DIR/storage/
 
 echo "upload log"
-date > $HOME/node/log/upload.log
+#date > $HOME/node/log/upload.log
 rsync -avzhe ssh --rsync-path=$REMOTE_RSYNC_PATH --progress ./log/* $BENCHTEST_DIR/log/
