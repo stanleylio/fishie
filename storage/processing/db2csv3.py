d08c094c78d1061fc92ecb7ad97f7c1f674a4842
Stanley Lio <stanleylio@gmail.com>
2016-02-03 08:33:22 +0000

    fixed the ordering of the columns in the csv output (time should be the first column)

--- storage/processing/db2csv3.py
+++ storage/processing/db2csv3.py
@@ -4,19 +4,20 @@ from storage import storage_read_only
 def dt2ts(dt):
     return calendar.timegm(dt.timetuple()) + (dt.microsecond)*(1e-6)
 
-node = 'node-009'
+node = 'node-011'
 
 store = storage_read_only()
 tmp = store.read_all(node)
 
+# need it for the ordering
+cols = store.get_list_of_columns(node)
+
 # convert Python datetime to POSIX timestamps
-time_col = 'Timestamp'
-if 'ReceptionTime' in tmp:
-    time_col = 'ReceptionTime'
+time_col = cols[0]  # the first column is time - a convention so far maintained
 tmp[time_col] = tuple(dt2ts(v) for v in tmp[time_col])
 
 # ah the one-liner sickness...
 with open(node + '.csv','w') as f:
-    f.write(','.join(tmp.keys()) + '\n')
-    f.write('\n'.join([','.join([str(v) for v in r]) for r in zip(*[tmp[k] for k in tmp.keys()])]))
+    f.write(','.join(cols) + '\n')
+    f.write('\n'.join([','.join([str(v) for v in r]) for r in zip(*[tmp[k] for k in cols])]))
 
