c8fd33103b1901a5fb910849bbff68a0246c0977
Stanley Lio <stanleylio@gmail.com>
2016-05-02 09:44:38 +0000

    .

--- config/poh/base_003.py
+++ config/poh/base_003.py
@@ -1,10 +1,10 @@
-
 id = 'base-003'
 site = 'poh'
 name = 'Base Station'
 location = 'Paepae o He\'ei, Kane\'ohe'
 google_earth_link = 'https://goo.gl/maps/ECEBgo3UEp82'
 note = 'Celeron-NUC (base_003)'
+public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzWObx6WFAfxuC3uDzAs2Ifq/rnxkhHQit6qKQ7RnuDfE3y8NmpyuZlWuYGHplNDCzXSILBXZTTA/rsrZuPfr6iKp8jNE6LRwf5dSx2jrY2pf7kXW+rWfSGFaAhHU73E/iZaOFBVccQmQFPsZPFN7n+sGMEDNI3bzMVsdZ+uKdv5FQArOSZISRpIol7HWJQ2mRdaZhqE7XgNrCugeihCunVusVlLnFaNomJnCy9yAxE3k5FmyUwGtYgDSY+zNEwCfXJasUfkxg4KIZrizZegr9o2bKi/5jCifMUpaeTvq4bfShe6AymZ2ADHPpyT6XWPxu4sHuOYVQ2GDIyeUl8CQX nuc@celeron-nuc'
 
 xbee_port = '/dev/ttyUSB0'
 xbee_baud = 115200
