785590e0e71c49baa18777d799844c0be1852e1a
Stanley Lio <stanleylio@gmail.com>
2016-05-23 21:20:40 +0000

    .

---
+++ config/coconut/base_002.py
@@ -0,0 +1,12 @@
+
+id = 'base-002'
+site = 'coconut'
+name = 'Base Station'
+location = 'Coconut Island'
+google_earth_link = 'https://goo.gl/maps/iGm1RfuWRot'
+note = 'BBB-based (base_002)'
+public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAnKsvPuLQ0b3LtS8YJ28FEEZyRfY5GbpQSbbWjqojBn2AhbXIBo6D7D2c2j2OOcRkh7+3dyX7nyCjn0Yojb21sZxrW26jmT+7Zbi4N88Hexqd20RIjKeECA5ahUq8Kk+vG9qZvozXUR4RLyopn8bQ5240WlenNUxD2am81SxJzfJsMWeQniff9uiCnab+EZrbTn/CxgQex9cvgrbsRtoEUQwpO9bnXJpPhjGZdF/1PfDmDJvsd0NUa/SU8xTCh7ASLfQIopvxq9GuRN2GyfnW9HO4r0YBuBKt4oixJJRVlRF6RLv0LHTjQVEuRmmPwTEsGkwywGPn/9asbS4oBPF5 root@base-002'
+
+xbee_port = '/dev/ttyO1'
+xbee_baud = 115200
+log_dir = '/home/nuc/node/log'
--- config/poh/base_002.py
+++
@@ -1,7 +0,0 @@
-
-id = 2
-name = 'Base Station #2'
-note = 'BBB-based (base_002)'
-xbee_port = '/dev/ttyO1'
-xbee_baud = 115200
-log_dir = './log'
