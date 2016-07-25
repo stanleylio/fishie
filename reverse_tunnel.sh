816423d9a4dcad8586d0c802d32dadba3ea286c0
Stanley Lio <stanleylio@gmail.com>
2016-07-25 18:29:37 +0000

    .

--- reverse_tunnel.sh
+++ reverse_tunnel.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #https://juliansimioni.com/blog/howto-access-a-linux-machine-behind-a-home-router-with-ssh-tunnels/
 
-HostA="128.171.153.115"
+HostA=128.171.153.115
 
 createTunnel() {
     /usr/bin/ssh -f -N -R 10023:localhost:22 -L19922:$HostA:22 $HostA
     if [[ $? -eq 0 ]]; then
-        echo Tunnel to HostA created successfully
+        echo Tunnel to $HostA created successfully
     else
-        echo An error occurred creating a tunnel to HostA. Return code: $?
+        echo An error occurred creating a tunnel to $HostA. Return code: $?
     fi
 }
 /usr/bin/ssh -p 19922 localhost ls > /dev/null
 if [[ $? -ne 0 ]]; then
-    echo Creating new tunnel connection to HostA
+    echo Creating new tunnel connection to $HostA
     createTunnel
 fi
