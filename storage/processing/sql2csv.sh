971f78a56cd347ff404c9f4a86d95a6f0286f5f2
Stanley Lio <stanleylio@gmail.com>
2016-09-12 22:56:14 +0000

    .

---
+++ storage/processing/sql2csv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 /home/nuc/node/www/poh/storage/sensor_data.db < sql2csv.sql
---
+++ storage/processing/sql2csv.sql
@@ -0,0 +1,32 @@
+.header on
+.mode csv
+
+.once node-001.csv
+SELECT * FROM node_001;
+
+.once node-002.csv
+SELECT * FROM node_002;
+
+.once node-003.csv
+SELECT * FROM node_003;
+
+.once node-004.csv
+SELECT * FROM node_004;
+
+.once node-007.csv
+SELECT * FROM node_007;
+
+.once node-008.csv
+SELECT * FROM node_008;
+
+.once node-009.csv
+SELECT * FROM node_009;
+
+.once node-025.csv
+SELECT * FROM node_025;
+
+.once node-021.csv
+SELECT * FROM node_021;
+
+.once node-022.csv
+SELECT * FROM node_022;
