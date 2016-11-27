a9dc0d7fb7feb802fdd5c054b362e57227433f05
Stanley Lio <stanleylio@gmail.com>
2016-11-27 05:55:17 +0000

    .

--- display/gen_plot.sh
+++ display/gen_plot.sh
@@ -14,13 +14,13 @@ python t0.py --site=poh --dbfile=$HOME/node/www/poh/storage/sensor_data.db --plo
 # plot filtered and boundary-checked
 # clone the database
 # wait, I forgot. what are these for?
-echo "PoH boundary-checked"
-rsync -avzhe ssh --progress $HOME/node/www/poh/storage/sensor_data.db* $HOME/node/www/poh/storage/bounded/
+#echo "PoH boundary-checked"
+#rsync -avzhe ssh --progress $HOME/node/www/poh/storage/sensor_data.db* $HOME/node/www/poh/storage/bounded/
 # replace out-of-bound values with Null
-python proc.py --site=poh --dbfile=$HOME/node/www/poh/storage/bounded/sensor_data.db
-chmod g+w $HOME/node/www/poh/storage/bounded/sensor_data.db
+#python proc.py --site=poh --dbfile=$HOME/node/www/poh/storage/bounded/sensor_data.db
+#chmod g+w $HOME/node/www/poh/storage/bounded/sensor_data.db
 # plot boundary-checked
-python t0.py --site=poh --dbfile=$HOME/node/www/poh/storage/bounded/sensor_data.db --plot_dir=$HOME/node/www/poh/bounded
+#python t0.py --site=poh --dbfile=$HOME/node/www/poh/storage/bounded/sensor_data.db --plot_dir=$HOME/node/www/poh/bounded
 
 
 # - - - - -
@@ -29,10 +29,10 @@ python t0.py --site=poh --dbfile=$HOME/node/www/poh/storage/bounded/sensor_data.
 echo "Coconut Island (Katie's)"
 python t0.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/sensor_data.db --plot_dir=$HOME/node/www/coconut
 
-echo "Coconut boundary-checked"
-rsync -avzhe ssh --progress $HOME/node/www/coconut/storage/sensor_data.db* $HOME/node/www/coconut/storage/bounded/
-python proc.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/bounded/sensor_data.db
-python t0.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/bounded/sensor_data.db --plot_dir=$HOME/node/www/coconut/bounded
+#echo "Coconut boundary-checked"
+#rsync -avzhe ssh --progress $HOME/node/www/coconut/storage/sensor_data.db* $HOME/node/www/coconut/storage/bounded/
+#python proc.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/bounded/sensor_data.db
+#python t0.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/bounded/sensor_data.db --plot_dir=$HOME/node/www/coconut/bounded
 
 
 # - - - - -
@@ -40,8 +40,8 @@ python t0.py --site=coconut --dbfile=$HOME/node/www/coconut/storage/bounded/sens
 # The infrasturcture does not support this yet. Hollie broke the
 # "one database file per site" assumption
 # - - - - -
-echo "Coconut Island (Hollie's)"
-python t0.py --site=coconut --dbfile=$HOME/data/htank/storage/sensor_data.db --plot_dir=$HOME/node/www/coconut
+#echo "Coconut Island (Hollie's)"
+#python t0.py --site=coconut --dbfile=$HOME/data/htank/storage/sensor_data.db --plot_dir=$HOME/node/www/coconut
 
 #echo "Coconut boundary-checked"
 #rsync -avzhe ssh --progress $HOME/node/www/htank/storage/sensor_data.db* $HOME/node/www/htank/storage/bounded/
