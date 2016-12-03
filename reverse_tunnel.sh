6266547cffcdf2af2ae3c11619fc3dee9c4a4bd9
Stanley Lio <stanleylio@gmail.com>
2016-12-03 02:18:57 +0000

    .

--- reverse_tunnel.sh
+++ reverse_tunnel.sh
@@ -5,6 +5,7 @@
 
 Host=128.171.153.115
 #Host=166.122.97.82
+RemotePort=10032
 
-autossh -M 19922 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R 10032:localhost:22 $(hostname)@$Host -p 22
+autossh -M 19922 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R $RemotePort:localhost:22 $(hostname)@$Host -p 22
 #autossh -M 19922 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R 10032:localhost:22 kmet-bbb2@166.122.97.82 -p 22
