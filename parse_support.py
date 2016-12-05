da7b0cbb6b4448d00d2d291c93061ec6eb4e92c4
Stanley Lio <stanleylio@gmail.com>
2016-12-05 10:10:38 +0000

    .

--- parse_support.py
+++ parse_support.py
@@ -29,10 +29,12 @@ def pretty_print(d):
         if isinstance(tmp,float):
             tmp = datetime.fromtimestamp(tmp)
         print('Sampled at {}'.format(tmp))
-    #what was I thinking...
     #for k in [k for k in sorted(d.keys()) if all([k != t for t in ['Timestamp','node','ReceptionTime']])]:
         #print('{}{}{}'.format(k,' '*(max_len + 4 - len(k)),d[k]))
-    for k in sorted(filter(lambda x: x not in ['Timestamp','node','ReceptionTime'],d.keys())):
+    #what was I thinking...
+    #for k in sorted(filter(lambda x: x not in ['Timestamp','node','ReceptionTime'],d.keys())):
+    #what was I thinking...
+    for k in sorted(set(d.keys()) - set(['Timestamp','node','ReceptionTime'])):
         print('{}{}{}'.format(k,' '*(max_len + 4 - len(k)),d[k]))
 
 def parse_message(line,site):
