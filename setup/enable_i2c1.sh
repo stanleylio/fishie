663975167a90c196018bfb0b68b43afeb9b05602
Stanley Lio <stanleylio@gmail.com>
2017-01-24 03:46:24 +0000

    updated enable i2c script

--- setup/enable_i2c1.sh
+++ setup/enable_i2c1.sh
@@ -2,6 +2,7 @@
 # enable the other I2C port on the BBB
 
 i2cdetect -l
-echo BB-I2C1 > /sys/devices/bone_capemgr.9/slots
-cat /sys/devices/bone_capemgr.9/slots
+#echo BB-I2C1 > /sys/devices/bone_capemgr.9/slots
+echo BB-I2C1 > /sys/devices/platform/bone_capemgr/slots
+cat /sys/devices/platform/bone_capemgr/slots
 i2cdetect -l
