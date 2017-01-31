589c3309130ea795eaacf2bd1bb6baad9a98fdb0
Stanley Lio <stanleylio@gmail.com>
2017-01-31 07:22:34 +0000

    replaced key "depth_meter" with "depth"

--- display/t5a.py
+++ display/t5a.py
@@ -11,11 +11,15 @@ from datetime import datetime,timedelta
 from node.display.gen_plot import plot_time_series
 
 
+def m2ft(m):
+    return 3.28084*m
+
+
 # makaha 1 weekly
 r = requests.get('http://grogdata.soest.hawaii.edu/poh/data/location/makaha1/depth.json?minutes=10080&max_count=1000')
 r = r.json()
-r['depth_meter'] = [tmp*3.28084 for tmp in r['depth_meter']]
-plot_time_series(r['ReceptionTime'],r['depth_meter'],\
+r['depth_feet'] = [m2ft(tmp) for tmp in r['depth']]    # now in feet
+plot_time_series(r['ReceptionTime'],r['depth_feet'],\
                  join('/var/www/uhcm/img/poh/node-009/weekly/depth_feet.png'),\
                  title='Water Depth at Makaha 1 (node-009)',\
                  ylabel='Feet',\
@@ -24,8 +28,8 @@ plot_time_series(r['ReceptionTime'],r['depth_meter'],\
 # makaha 1 monthly
 r = requests.get('http://grogdata.soest.hawaii.edu/poh/data/location/makaha1/depth.json?minutes=43200&max_count=1000')
 r = r.json()
-r['depth_meter'] = [tmp*3.28084 for tmp in r['depth_meter']]
-plot_time_series(r['ReceptionTime'],r['depth_meter'],\
+r['depth_feet'] = [m2ft(tmp) for tmp in r['depth']]    # now in feet
+plot_time_series(r['ReceptionTime'],r['depth_feet'],\
                  join('/var/www/uhcm/img/poh/node-009/monthly/depth_feet.png'),\
                  title='Water Depth at Makaha 1 (node-009)',\
                  ylabel='Feet',\
@@ -34,8 +38,8 @@ plot_time_series(r['ReceptionTime'],r['depth_meter'],\
 # makaha 2 weekly
 r = requests.get('http://grogdata.soest.hawaii.edu/poh/data/location/makaha2/depth.json?minutes=10080&max_count=1000')
 r = r.json()
-r['depth_meter'] = [tmp*3.28084 for tmp in r['depth_meter']]
-plot_time_series(r['ReceptionTime'],r['depth_meter'],\
+r['depth_feet'] = [m2ft(tmp) for tmp in r['depth']]    # now in feet
+plot_time_series(r['ReceptionTime'],r['depth_feet'],\
                  join('/var/www/uhcm/img/poh/node-008/weekly/depth_feet.png'),\
                  title='Water Depth at Makaha 2 (node-008)',\
                  ylabel='Feet',\
@@ -44,8 +48,8 @@ plot_time_series(r['ReceptionTime'],r['depth_meter'],\
 # makaha 2 monthly
 r = requests.get('http://grogdata.soest.hawaii.edu/poh/data/location/makaha2/depth.json?minutes=43200&max_count=1000')
 r = r.json()
-r['depth_meter'] = [tmp*3.28084 for tmp in r['depth_meter']]
-plot_time_series(r['ReceptionTime'],r['depth_meter'],\
+r['depth_feet'] = [m2ft(tmp) for tmp in r['depth']]    # now in feet
+plot_time_series(r['ReceptionTime'],r['depth_feet'],\
                  join('/var/www/uhcm/img/poh/node-008/monthly/depth_feet.png'),\
                  title='Water Depth at Makaha 2 (node-008)',\
                  ylabel='Feet',\
