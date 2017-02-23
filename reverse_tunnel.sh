a81db39b33250783835140eb3ea44316831d4b2c
Stanley Lio <stanleylio@gmail.com>
2017-02-23 21:09:06 +0000

    .

--- reverse_tunnel.sh
+++ reverse_tunnel.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 #https://raymii.org/s/tutorials/Autossh_persistent_tunnels.html
 
-# of course, sudo apt install autossh and do the ssh-copy-id dance from the remote host first.
+# sudo apt install autossh
+# create user for this node on the remote host
+#sudo useradd -m kmet-bbb3
+#sudo passwd kmet-bbb3
+#sudo chsh -s /bin/bash kmet-bbb3
+# do the ssh-copy-id dance
 
+RemotePort=10033
+
+# glazerlab-i7nuc
 Host=128.171.153.115
-#Host=166.122.97.82
-RemotePort=10032
-
 autossh -M 19922 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R $RemotePort:localhost:22 $(hostname)@$Host -p 22
-#autossh -M 19922 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R 10032:localhost:22 kmet-bbb2@166.122.97.82 -p 22
+
+# otg-met
+#Host=166.122.96.11
+#autossh -M 19923 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R $RemotePort:localhost:22 $(hostname)@$Host -p 22
+
+# kmet-rpi1
+#Host=166.122.96.119
+#autossh -M 19924 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -R $RemotePort:localhost:22 $(hostname)@$Host -p 22
