7648a78bc97479a453fcfb12fbcf6aa792245993
Stanley Lio <stanleylio@gmail.com>
2017-03-18 00:35:08 +0000

    .

--- send2server.py
+++ send2server.py
@@ -1,6 +1,8 @@
-# Send a string to i7NUC via HTTP POST
-# Messages are signed by the private key of this device
-# Messages are verified by i7NUC with the public key of this device
+# Send a string to remote host via HTTP POST
+# Messages are signed by the private key of the sender device
+# Messages are verified by remote host with the public key of the sender device
+#
+# Demo: If arguments are supplied, they are sent as individual messages to glazerlab-i7nuc.
 #
 # Targeted at the v4 API
 #
@@ -8,22 +10,21 @@
 # hlio@hawaii.edu
 # University of Hawaii
 # All Rights Reserved. 2016
-import requests,time
+import requests,time,socket
 from authstuff import get_signature
 from os.path import expanduser,join
-import socket
 
 
 def prepare_message(m):
     """Sign, date, add own ID. Return as a dict()"""
-    #privatekey = open('/home/nuc/.ssh/id_rsa').read()
-    privatekey = open(join(expanduser('~'),'.ssh/id_rsa')).read()
-    sig = get_signature(m,privatekey)
+    sig = get_signature(m,prepare_message.privatekey)
     return {'src':socket.gethostname(),
             'ts':time.time(),
             'msg':m,
             'sig':sig,
             }
+prepare_message.privatekey = open(join(expanduser('~'),'.ssh/id_rsa')).read().strip()
+
 
 def post(m,endpoint):
     r = requests.post(endpoint,data=prepare_message(m))
@@ -40,7 +41,7 @@ if '__main__' == __name__:
             print(post(m,url))
     else:
         print(time.time())
-        for i in range(1000):
-            m = '"It is difficult to get a man to understand something, when his salary depends upon his not understanding it." "Surely, comrades, you don\'t want Jones back?" "Two possibilities exist: either we are alone in the universe, or we are not. Both are equally terrifying."'
+        for i in range(1):
+            m = '"single digit millionaires have no effective access to the legal system" "It is difficult to get a man to understand something, when his salary depends upon his not understanding it." "Surely, comrades, you don\'t want Jones back?" "Two possibilities exist: either we are alone in the universe, or we are not. Both are equally terrifying."'
             post(m,url)
         print(time.time())
