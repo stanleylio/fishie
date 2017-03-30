c93c561c72e2c009647f0e4f3a77c97af16fed93
Stanley Lio <stanleylio@gmail.com>
2017-03-30 19:39:45 +0000

    .

--- setup/backup_settings.sh
+++ setup/backup_settings.sh
@@ -15,11 +15,15 @@ cp /boot/uEnv.txt $DIR/uEnv.txt
 cp -a /etc/network/interfaces $DIR/interfaces.txt
 cp -a /etc/hostname $DIR/hostname.txt
 cp -a /etc/hosts $DIR/hosts.txt
-cp -a /etc/wpa_supplicant/wpa_supplicant.conf $DIR/wpa_supplicant.conf
 #cp -a /etc/resolv.conf $DIR/resolv.conf.txt
 cat /etc/resolv.conf > $DIR/resolv.conf.txt
 cp -a /etc/rc.local $DIR/rc.local.txt
 cp -a /etc/ntp.conf $DIR/ntp.conf
+
+if [ -a /etc/wpa_supplicant ]
+then
+	rsync -avh /etc/wpa_supplicant $DIR
+fi
 
 #cp -a /etc/supervisor/supervisord.conf $DIR/supervisord.conf
 #cp -a /etc/supervisor/conf.d/sampling.conf $DIR/sampling.conf
