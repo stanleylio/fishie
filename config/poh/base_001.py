0068a782983173d98d456be79eddd47dc7cf2ed8
Stanley Lio <stanleylio@gmail.com>
2017-04-04 21:55:25 +0000

    updating base-001

--- config/poh/base_001.py
+++ config/poh/base_001.py
@@ -4,7 +4,7 @@ google_earth_link = 'https://goo.gl/maps/ECEBgo3UEp82'
 note = 'BBB-based (base_001)'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDxQo/IVvzvYQJNif7XDD2UOwrQjbY6azURNA4K6QU7xMvDGJ7cohkib5sneCnLiL836VlT3AJ3Nj1cM/0UftJv1h8H836jfSvtTTdDhDwDg150c32VRq1AN8yoUyQMKsfKduuAAcdpRtTnVO3brNHn9o+pn/p36uepKF+kZUVKf75Bh9VqCldAzCbYx+jhWyRItpEKYqOftlcyyoM1GCIx+1R1143qR+onN1wSa2+N8KO6XFN0lFmaVAUC4guRffESMg6GS12GuJLT8iOYhDMFeMrjS9/Fn14zW7oRIungxHGYPYXsju1UmxaArtWfqj2wK/mioqZFktKUg9IT7Ex root@base-001'
 
-logdir = '/var/uhcm/log'
+log2txt_output_path = '/var/uhcm/log'
 dbfile = '/var/uhcm/storage/sensor_data.db'
 
 subscribeto = ['127.0.0.1:9002']
