aaaf4c16c45cc6bced5c04f4241834ecb64b7f1f
Stanley Lio <stanleylio@gmail.com>
2017-04-08 09:03:59 +0000

    .

--- log2txt.py
+++ log2txt.py
@@ -1,3 +1,5 @@
+# store stuff from zmq to text file, with timestamps
+# 
 # Stanley H.I. Lio
 # hlio@hawaii.edu
 # University of Hawaii
@@ -15,7 +17,8 @@ config = import_node_config()
 
 
 # product of this script, the raw text file
-output_path = config.log2txt_output_path
+output_path = getattr(config,'log2txt_output_path',None)
+assert output_path is not None and exists(output_path)
 
 
 #'DEBUG,INFO,WARNING,ERROR,CRITICAL'
@@ -34,7 +37,16 @@ logger.addHandler(handler)
 topic = u''
 context = zmq.Context()
 zsocket = context.socket(zmq.SUB)
-for feed in config.subscribeto:
+# Still not foolproof. Imagine power to both publisher and subscriber got cycled.
+# publisher may come up after subscriber. This script should not proceed if
+# no publisher is found. The static config is still needed.
+# Also, how often should subscribers initiate a new search?
+#a = set(config.subscribeto)         # feeds found in config
+a = set(getattr(config,'subscribeto',[]))
+b = set(get_publisher_list(topic))  # feeds found in the network
+feeds = a.union(b)
+assert len(feeds) > 0,'No ZMQ feed defined/found.'
+for feed in feeds:
     feed = 'tcp://' + feed
     logger.info('subscribing to ' + feed)
     zsocket.connect(feed)
@@ -51,14 +63,16 @@ with open(join(output_path,'raw.txt'),'a',0) as raw,\
             socks = dict(poller.poll(1000))
             if zsocket in socks and zmq.POLLIN == socks[zsocket]:
                 m = zsocket.recv()
+                print('= = = = = = = = = =')
                 print(m)
+                
                 raw.write(m)
-
                 dt = datetime.utcnow()
                 tsraw.write('{}\t{:6f}\t{}\n'.format(dt.isoformat(),dt2ts(dt),m.strip()))
 
                 raw.flush()
                 tsraw.flush()
+                
         except KeyboardInterrupt:
             logger.info('user interrupted')
             break
