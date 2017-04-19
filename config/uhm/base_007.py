52b334090261880f4782b2712ec4d8dd384ba6ba
Stanley Lio <stanleylio@gmail.com>
2017-04-19 01:56:02 +0000

    .

--- config/uhm/base_007.py
+++ config/uhm/base_007.py
@@ -1,9 +1,99 @@
-name = 'Sandbox Base Station'
-location = 'MSB UHM'
+name = 'Tempbath Station'
+location = 'MSB301, UHM'
 google_earth_link = '#'
-note = 'Wifi bone (base-007)'
-public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClU029aEkY4HAp2E/ZCuwWA15yCGZm16sTzlp4TYdF5pQAf4yT80pnM3/CaU0sJV6W6ZQsOgUv3Xlsur1Hvf7lu2rP9ISTTXYXujQvr+SlnsHB1Z6MaGje8DpZAmIODY+sk2BIKpoF6kgMKnWepz+S6Of/b/5+qymFgfu+sYzMupGZZ5qJtFgJyWcIRqtOjVCi3sv6JePwjgZRbJ6VTfPqBDJDCyvWnECM9nTaHCKzxr/HGi7uKRWmU2RpZv9n7QfU0jgMNbsF8pwlBWmlxnp7CMkNOWiiWCucgKNV5UMdmu+iy8njP77AeQdNuP98ZRIK1RQOlyH7p6l3NXUrreeP root@base-007'
+note = 'Wifi bone; cape v0.2; base-007'
+public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9r/ROdI2KoSGWsizk8fhcUWONAMpAQsxJF8v8qyhZBVBGYT54bHWeTogcSl+cKmocqEvEY9HoL5APmqCGMy7k7MAKNIuN2AoEJm6bBia50vdtIK1LpMalHQ/dwaghsTU5a0nbC6eUiMUzTChiUSpzO3vH7Gj4hYITUwbpOlWTW5NVE7iRRSb/CiXu22m1hMuR5cyFzUzap5cQDmWULFCNyq4sSI6TZJru41kgrgs+x1UkdwOZmXDAgGmxqv8Pqj9WRWvd2KZNa6bXR+D7PfqYO6d+IvuS1eu2mD3WT6mqPIx2Om3He5m/GjVPTWlNkHExSAOpbZfMOsn9lrIeKtGX nuc@base-007'
 
 subscribeto = ['127.0.0.1:9002']
 log2txt_output_path = '/var/uhcm/log'
-#dbfile = '/var/uhcm/storage/sensor_data.db'
+
+
+conf = [
+    {
+        'dbtag':'ts',
+        'dbtype':'DOUBLE NOT NULL',
+        'description':'Time of sampling',
+        'plot':False,
+    },
+    {
+        'dbtag':'tsys0',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH0 TSYS01',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'si0',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH0 Si7051',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'tsys1',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH1 TSYS01',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'si1',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH1 Si7051',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'tsys2',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH2 TSYS01',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'si2',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH2 Si7051',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'tsys3',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH3 TSYS01',
+        'lb':-15,
+        'ub':60,
+    },
+    {
+        'dbtag':'si3',
+        'dbtype':'DOUBLE',
+        'unit':'Deg.C',
+        'description':'CH3 Si7051',
+        'lb':-15,
+        'ub':60,
+    },
+]
+
+
+if '__main__' == __name__:
+    for c in conf:
+        print '- - -'
+        for k,v in c.iteritems():
+            print k, ':' ,v
+
+    import sys
+    sys.path.append('../..')
+    from storage.storage2 import create_table
+    #conf.insert(0,{'dbtag':'ReceptionTime','dbtype':'DOUBLE NOT NULL'})
+    create_table(conf,__file__.split('.')[0].replace('_','-'))
+
+    #from os import makedirs
+    #makedirs(log2txt_output_path)
+    
