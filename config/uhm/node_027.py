3c3dc4a9bb45e6a4075c5865241d7181eabc2f3b
Stanley Lio <stanleylio@gmail.com>
2017-04-25 04:21:34 +0000

    .

--- config/uhm/node_027.py
+++ config/uhm/node_027.py
@@ -96,10 +96,8 @@ if '__main__' == __name__:
 
     import sys
     sys.path.append('../..')
+    from os.path import basename
     from storage.storage2 import create_table
-    #conf.insert(0,{'dbtag':'ReceptionTime','dbtype':'DOUBLE NOT NULL'})
-    create_table(conf,__file__.split('.')[0].replace('_','-'))
 
-    #from os import makedirs
-    #makedirs(log2txt_output_path)
-    
+    conf.insert(0,{'dbtag':'ReceptionTime','dbtype':'DOUBLE NOT NULL'})
+    create_table(conf,basename(__file__).split('.')[0].replace('_','-'))
