5fc81e44694b74bdea6c36120ecbcfa477ea186b
Stanley Lio <stanleylio@gmail.com>
2017-05-04 16:28:57 -1000

    .

--- config/poh/base_004.py
+++ config/poh/base_004.py
@@ -5,7 +5,7 @@ note = 'Celeron-NUC2 (base-004)'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuAIuai8QXOsi2KTzJrNVBFXvWMvqUQ5QKGYVzo7/Qr6gxj8xZZCcdukFFkTrCvHEsl7J0gHNsdwakTs+jVw8F7km4MPozGAMXoDEV/wWcXIxoaObUjRYsoUjArWrjsw3HlMbM+juKCYOPQsk8nYLpqv3BQ0Vc6p36OVDmIUfqNpwYxb6lpjgGo6j3npg+zgc8wfa5OyOBSEB5nkYN9k2WvJlKRVtDgvb4LA1lvTthPwb8z8jwFI2AV/Dr5SB1+miTQKbLmav/R6Uzpgxlc8mAMg9xz4NVQYm5t/uuCfigSG9S7oYg2FewNElO3zvykuTYHlVlFK6m55LaTE0gLSPr nuc@base-004'
 
 subscribeto = ['127.0.0.1:9002']
-sampling_serial_ports = [('/dev/ttyUSB0',115200)]
+#sampling_serial_ports = [('/dev/ttyUSB0',115200)]
 log2txt_output_path = '/var/uhcm/log'
 zmq2http_url = 'https://grogdata.soest.hawaii.edu/api/5/uhcm'
 dbfile = '/var/uhcm/storage/sensor_data.db'
