7fc8233dfc43c0885b046041aec1c6c9d6790a3d
Stanley Lio <stanleylio@gmail.com>
2017-05-06 21:03:43 +0000

    .

--- config/poh/base_003.py
+++ config/poh/base_003.py
@@ -7,7 +7,7 @@ note = 'Celeron-NUC (base-003)'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzWObx6WFAfxuC3uDzAs2Ifq/rnxkhHQit6qKQ7RnuDfE3y8NmpyuZlWuYGHplNDCzXSILBXZTTA/rsrZuPfr6iKp8jNE6LRwf5dSx2jrY2pf7kXW+rWfSGFaAhHU73E/iZaOFBVccQmQFPsZPFN7n+sGMEDNI3bzMVsdZ+uKdv5FQArOSZISRpIol7HWJQ2mRdaZhqE7XgNrCugeihCunVusVlLnFaNomJnCy9yAxE3k5FmyUwGtYgDSY+zNEwCfXJasUfkxg4KIZrizZegr9o2bKi/5jCifMUpaeTvq4bfShe6AymZ2ADHPpyT6XWPxu4sHuOYVQ2GDIyeUl8CQX nuc@celeron-nuc'
 
 subscribeto = ['127.0.0.1:9002']
-sampling_serial_ports = [('/dev/ttyUSB0',115200),('/dev/ttyUSB1',115200)]
+#sampling_serial_ports = [('/dev/ttyUSB0',115200),('/dev/ttyUSB1',115200)]
 log2txt_output_path = '/var/uhcm/log'
 #logdir = '/var/uhcm/log'
 dbfile = '/var/uhcm/storage/sensor_data.db'
