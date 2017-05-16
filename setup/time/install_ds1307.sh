72c95f461feee30d72781fa36412a341bd2efa86
Stanley Lio <stanleylio@gmail.com>
2017-05-16 21:46:10 +0000

    .

--- setup/time/install_ds1307.sh
+++ setup/time/install_ds1307.sh
@@ -12,11 +12,13 @@ echo "Installing external RTC (DS1307/DS3231)..."
 #	echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 #fi
 if [ -f /sys/class/i2c-adapter/i2c-2 ]; then
-	i2cdetect -y -r 2
+	sudo i2cdetect -y -r 2
 	echo "using i2c-2"
+	sudo -i
 	echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-2/new_device
+	exit
 fi
-hwclock -r -f /dev/rtc1
+sudo hwclock -r -f /dev/rtc1
 
 #cat rc.local.txt > /etc/rc.local
 
@@ -25,8 +27,9 @@ echo "Getting time via NTP..."
 #ntpdate -b -s -u pool.ntp.org
 #ntpdate ntp.soest.hawaii.edu
 #sudo service ntp stop
+sudo echo "server ntp.soest.hawaii.edu" >> /etc/ntp.conf
 sudo systemctl stop ntp
-ntpd -gq
+sudo ntpd -gq
 #sudo service ntp start
 sudo systemctl start ntp
 #date -s "10 SEP 2015 22:00:30"
@@ -35,23 +38,22 @@ date
 
 # write to RTC
 echo "Setting BBB rtc..."
-hwclock --systohc --rtc=/dev/rtc0
-hwclock --show -f /dev/rtc0
+sudo hwclock --systohc --rtc=/dev/rtc0
+sudo hwclock --show -f /dev/rtc0
 
 echo "Setting external rtc..."
-hwclock --systohc --rtc=/dev/rtc1
-hwclock --show --rtc=/dev/rtc1
+sudo hwclock --systohc --rtc=/dev/rtc1
+sudo hwclock --show --rtc=/dev/rtc1
 
 #chmod +x clock_init.sh
 
 echo "Installing service..."
-cp ~/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307.service
-cp /home/nuc/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307.service
-systemctl enable rtc-ds1307.service
-systemctl start rtc-ds1307.service
+sudo -i
+sudo cp ~/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307.service
+sudo cp /home/nuc/node/setup/time/rtc-ds1307.service /lib/systemd/system/rtc-ds1307.service
+sudo systemctl enable rtc-ds1307.service
+sudo systemctl start rtc-ds1307.service
 #cp ~/node/setup/time/rtc-ds1307-rpi2.service /lib/systemd/system/rtc-ds1307-rpi2.service
 #systemctl enable rtc-ds1307-rpi2.service
 #systemctl start rtc-ds1307-rpi2.service
-
-# shutdown -r now
-exit 0
+exit
