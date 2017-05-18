b9325c88c9a60ada99ad666b2525b5b14a43b85a
Stanley Lio <stanleylio@gmail.com>
2017-05-18 23:39:24 +0000

    .

--- rabbit2stdout.py
+++ rabbit2stdout.py
@@ -3,33 +3,28 @@
 # hlio@hawaii.edu
 # University of Hawaii
 # All Rights Reserved, 2017
-import pika,socket,traceback,sys,time,math,MySQLdb,logging,argparse
+import pika,traceback,sys,logging,argparse
 from os.path import expanduser,basename
 sys.path.append(expanduser('~'))
-from node.parse_support import parse_message,pretty_print
-from node.storage.storage2 import storage
 from cred import cred
 
 
 logging.basicConfig(level=logging.INFO)
 
+
 parser = argparse.ArgumentParser(description="""Redirect RabbitMQ exchange UHCM to STDOUT. Example: python rabbit2stdout.py glazerlab-e5 base-004 node-027""")
 parser.add_argument('sources',type=str,nargs='*')
 args = parser.parse_args()
 
-
 exchange = 'uhcm'
-nodeid = socket.gethostname()
 sources = args.sources
 
 credentials = pika.PlainCredentials('nuc',cred['rabbitmq'])
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel = connection.channel()
-
 channel.exchange_declare(exchange=exchange,type='topic',durable=True)
 result = channel.queue_declare(queue=basename(__file__),
                                exclusive=True)
-
 queue_name = result.method.queue
 
 
