ab4e9a520f9bbcf28ddd97bba06ab0b87c8b3448
Stanley Lio <stanleylio@gmail.com>
2017-06-01 11:10:13 -1000

    added base-005

--- config/poh/base_003.py
+++ config/poh/base_003.py
@@ -1,13 +1,12 @@
 #id = 'base-003'
 #site = 'poh'
 name = 'PoH Base Station'
-location = 'Paepae o He\'eia, Kane\'ohe, Oahu, Hawaii'
+location = 'Paepae o He\'eia, Oahu, Hawaii'
 google_earth_link = 'https://goo.gl/maps/ECEBgo3UEp82'
-note = 'Celeron-NUC (base-003)'
+note = 'NUC5CPYH (base-003)'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzWObx6WFAfxuC3uDzAs2Ifq/rnxkhHQit6qKQ7RnuDfE3y8NmpyuZlWuYGHplNDCzXSILBXZTTA/rsrZuPfr6iKp8jNE6LRwf5dSx2jrY2pf7kXW+rWfSGFaAhHU73E/iZaOFBVccQmQFPsZPFN7n+sGMEDNI3bzMVsdZ+uKdv5FQArOSZISRpIol7HWJQ2mRdaZhqE7XgNrCugeihCunVusVlLnFaNomJnCy9yAxE3k5FmyUwGtYgDSY+zNEwCfXJasUfkxg4KIZrizZegr9o2bKi/5jCifMUpaeTvq4bfShe6AymZ2ADHPpyT6XWPxu4sHuOYVQ2GDIyeUl8CQX nuc@celeron-nuc'
 
 subscribeto = ['127.0.0.1:9002']
 #sampling_serial_ports = [('/dev/ttyUSB0',115200),('/dev/ttyUSB1',115200)]
 log2txt_output_path = '/var/uhcm/log'
-#logdir = '/var/uhcm/log'
 dbfile = '/var/uhcm/storage/sensor_data.db'
