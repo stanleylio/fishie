164070814fee4b3c15e6bce842d59373d1cc1dd5
Stanley Lio <stanleylio@gmail.com>
2017-06-15 01:56:36 +0000

    .

---
+++ config/poh/node_001.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+#tag = 'node-001'
+name = 'CTD + optode'
+location = 'Triple Mākāhā b'
+note = 'Aanderaa 4330f + MS5803-14BA + EZO EC + BMP180'
+
+log_dir = './log'
+plot_dir = '../www'
+
+plot_range = 24*7
+data_source = '/home/nuc/node/www/poh/storage/sensor_data.db'
+
+xbee_port = '/dev/ttyO1'
+xbee_baud = 115200
+optode_port = '/dev/ttyO2'
+
+wait = 591
+
+multi_sample = 7
+
+#import sys
+#sys.path.append('..')
+from node.config.config_support import Range
+
+conf = [
+    {
+        'dbtag':'Timestamp',
+        'dbtype':'TIMESTAMP',
+        'comtag':'ts',
+        'unit':None,
+        'description':'Time of sampling',
+        'plot':False,
+    },
+    {
+        'dbtag':'P_180',
+        'dbtype':'REAL',
+        'comtag':'P_180',
+        'unit':'Pa',
+        'description':'Barometric pressure (BMP180)',
+        'plot':True,
+        'range':Range(80e3,110e3),
+        #'convf':lambda (x): x/1e3,
+        #'convunit':'kPa',
+    },
+    {
+        'dbtag':'T_180',
+        'dbtype':'REAL',
+        'comtag':'T_180',
+        'unit':'Deg.C',
+        'description':'Enclosure temperature (BMP180)',
+        'plot':True,
+        'range':Range(-10,80),
+    },
+    {
+        'dbtag':'P_5803',
+        'dbtype':'REAL',
+        'comtag':'P_5803',
+        'unit':'kPa',
+        'description':'Water pressure (MS5803-14BA)',
+        'plot':True,
+        'range':Range(80,150),
+    },
+    {
+        'dbtag':'T_5803',
+        'dbtype':'REAL',
+        'comtag':'T_5803',
+        'unit':'Deg.C',
+        'description':'Water Temperature (MS5803-14BA)',
+        'plot':True,
+        'range':Range(-10,60),
+    },
+    {
+        'dbtag':'ec',
+        'dbtype':'REAL',
+        'comtag':'ec',
+        'unit':'uS/cm',
+        'description':'Conductivity (EZO EC)',
+        'plot':False,
+        'range':Range(0,55e3),
+    },
+    {
+        'dbtag':'sal',
+        'dbtype':'REAL',
+        'comtag':'sal',
+        'unit':'(ppt)',
+        'description':'Salinity (EZO EC)',
+        'plot':False,
+        'range':Range(lb=0),
+    },
+    {
+        'dbtag':'O2Concentration',
+        'dbtype':'REAL',
+        'comtag':'O2',
+        'unit':'uM',
+        'description':'Oxygen concentration (4330F)',
+        'plot':True,
+        'range':Range(0,450),
+    },
+    {
+        'dbtag':'AirSaturation',
+        'dbtype':'REAL',
+        'comtag':'Air',
+        'unit':'%',
+        'description':'Air saturation (4330F)',
+        'plot':True,
+        'range':Range(0,150),
+    },
+    {
+        'dbtag':'Temperature',
+        'dbtype':'REAL',
+        'comtag':'T_4330f',
+        'unit':'Deg.C',
+        'description':'Water temperature (4330F)',
+        'plot':True,
+        'range':Range(-10,60),
+    },
+    {
+        'dbtag':'CalPhase',
+        'dbtype':'REAL',
+        'comtag':'CalPhase',
+        'unit':'Deg',
+        'description':'CalPhase',
+        'plot':False,
+    },
+    {
+        'dbtag':'TCPhase',
+        'dbtype':'REAL',
+        'comtag':'TCPhase',
+        'unit':'Deg',
+        'description':'TCPhase',
+        'plot':False,
+    },
+    {
+        'dbtag':'C1RPh',
+        'dbtype':'REAL',
+        'comtag':'C1RPh',
+        'unit':'Deg',
+        'description':'C1RPh',
+        'plot':False,
+    },
+    {
+        'dbtag':'C2RPh',
+        'dbtype':'REAL',
+        'comtag':'C2RPh',
+        'unit':'Deg',
+        'description':'C2RPh',
+        'plot':False,
+    },
+    {
+        'dbtag':'C1Amp',
+        'dbtype':'REAL',
+        'comtag':'C1Amp',
+        'unit':'mV',
+        'description':'C1Amp',
+        'plot':False,
+    },
+    {
+        'dbtag':'C2Amp',
+        'dbtype':'REAL',
+        'comtag':'C2Amp',
+        'unit':'mV',
+        'description':'C2Amp',
+        'plot':False,
+    },
+    {
+        'dbtag':'RawTemp',
+        'dbtype':'REAL',
+        'comtag':'RawTemp',
+        'unit':'mV',
+        'description':'RawTemp',
+        'plot':False,
+    },
+]
+
+
+if '__main__' == __name__:
+    for c in conf:
+        print '- - -'
+        for k,v in c.iteritems():
+            print k, ':' ,v
+
