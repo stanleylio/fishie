cbc9a485c9fdb3231f8799f56e43c49b8eec4c64
Stanley Lio <stanleylio@gmail.com>
2017-06-23 08:23:04 +0000

    .

---
+++ config/uhm/base_006.py
@@ -0,0 +1,7 @@
+name = '(noname)'
+location = '(anywhere)'
+google_earth_link = '#'
+note = 'bbb-based'
+public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqAi8zlZtVqegzL5kRHsfdUxCTAo/vYNhG+Avk53NaiNALVawvKR9asNstHi+sF/gtYVe7K+Db90lRMEARc+8Y1PCIlGlcwQFFzEA9xgV/xrJmJTJZE1vHm3Uf9ol6fQscBZ01NeppeSJZJsUir2X2aHcqcpcTspX7kGIiQBDi6VjSX4EULdMmHGg5/ruWlkOXDUN+0Lp5y41X5LVcKtYHtrU9usYZR5H3o6ruplsvBnDuLBrEQF4tYIowt7cxDYA6abeCtZacMtGIHvYdwceMyYZZhMN1HtIrLjUDyfcMlXeCVkytOfF4lXMckXLxPSBYu4i4Jmv6XpNGYMffMURx nuc@base-006'
+
+log2txt_output_path = '/var/uhcm/log'
