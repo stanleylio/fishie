ab5628245803ab585fa2e753ed9c808cd7ceaa84
Stanley Lio <stanleylio@gmail.com>
2017-10-11 01:31:51 +0000

    .

--- config/uhm/base_005.py
+++ config/uhm/base_005.py
@@ -9,21 +9,25 @@ conf = [
     {
         'dbtag':'system_clock',
         'description':'Device clock',
+        'interval':1*60,
     },
     {
         'dbtag':'uptime_second',
         'description':'Uptime in seconds',
+        'interval':1*60,
     },
     {
         'dbtag':'usedMB',
         'unit':'MB',
         'description':'Used disk space',
+        'interval':1*60,
     },
     {
         'dbtag':'freeMB',
         'unit':'MB',
         'description':'Remaining free disk space',
         'lb':10000,
+        'interval':1*60,
     },
 ]
 
