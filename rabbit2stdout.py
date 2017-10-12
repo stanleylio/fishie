fbc8fb2ae9f1843b2c89c7691658b8c052d15c03
Stanley Lio <stanleylio@gmail.com>
2017-10-12 09:34:23 +0000

    minor cleanup

--- rabbit2stdout.py
+++ rabbit2stdout.py
@@ -10,48 +10,47 @@ from cred import cred
 
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger('pika').setLevel(logging.WARNING)
 
 
 nodeid = socket.gethostname()
 
 
-parser = argparse.ArgumentParser(description="""Redirect RabbitMQ exchange UHCM to STDOUT. Example: python rabbit2stdout.py glazerlab-e5.samples base-004.samples""")
-parser.add_argument('sources',type=str,nargs='*')
+parser = argparse.ArgumentParser(description="""Show uhcm traffic. Example: python rabbit2stdout.py \"base-003.samples\" \"*.debug\"""")
+parser.add_argument('topics',type=str,nargs='*')
 args = parser.parse_args()
 
 exchange = 'uhcm'
-sources = args.sources
+topics = args.topics
 
 credentials = pika.PlainCredentials(nodeid,cred['rabbitmq'])
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
 channel = connection.channel()
-channel.exchange_declare(exchange=exchange,type='topic',durable=True)
-result = channel.queue_declare(queue=basename(__file__),
-                               exclusive=True)
+#channel.exchange_declare(exchange=exchange,type='topic',durable=True)
+result = channel.queue_declare(exclusive=True,auto_delete=True)
 queue_name = result.method.queue
 
 
-if len(sources) <= 0:
-    logging.info('No source specified. Listening to everything.')
+if len(topics) <= 0:
+    logging.info('No topic specified. Listening to everything.')
+    topics = ['#']
+
+for topic in topics:
     channel.queue_bind(exchange=exchange,
                        queue=queue_name,
-                       routing_key='#')
-else:
-    for source in sources:
-        channel.queue_bind(exchange=exchange,
-                           queue=queue_name,
-                           routing_key=source)
-                           #routing_key=source + '.samples')
+                       routing_key=topic)
 
 def callback(ch,method,properties,body):
-    print(method.routing_key,body)
-    #ch.basic_ack(delivery_tag=method.delivery_tag)
+    print('{}\t{}'.format(method.routing_key,body.strip()))
+    #print(body.strip())
+    #yield ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-logging.info(__file__ + ' is ready')
-channel.basic_consume(callback,queue=queue_name,no_ack=True)
+logging.debug(__file__ + ' is ready')
+channel.basic_consume(callback,queue=queue_name,exclusive=True,no_ack=True)
 try:
     channel.start_consuming()
 except KeyboardInterrupt:
-    logging.info('user interrupted')
-logging.info(__file__ + ' terminated')
+    logging.debug('user interrupted')
+connection.close()
+logging.debug(__file__ + ' terminated')
--- rabbit2stdout2.py
+++ rabbit2stdout2.py
@@ -1,24 +1,30 @@
-import pika,sys,logging
+import pika,sys,logging,socket
 from os.path import basename,expanduser
 sys.path.append(expanduser('~'))
 from pika import exceptions
 from pika.adapters import twisted_connection
 from twisted.internet import defer,reactor,protocol,task
+from twisted.internet.defer import setDebugging
 from cred import cred
 
 
 logging.basicConfig(level=logging.WARNING)
+logging.getLogger('pika').setLevel(logging.WARNING)
+setDebugging(True)
+
+
+nodeid = socket.gethostname()
 exchangename = 'uhcm'
 
 
 @defer.inlineCallbacks
 def run(connection):
     channel = yield connection.channel()
-    exchange = yield channel.exchange_declare(exchange=exchangename,type='topic',durable=True)
-    queue = yield channel.queue_declare(queue=basename(__file__),auto_delete=True,exclusive=True)
-    yield channel.queue_bind(exchange=exchangename,queue=basename(__file__),routing_key='#')
+    #exchange = yield channel.exchange_declare(exchange=exchangename,type='topic',durable=True)
+    result = yield channel.queue_declare(auto_delete=True,exclusive=True)
+    yield channel.queue_bind(exchange=exchangename,queue=result.method.queue,routing_key='#')
     yield channel.basic_qos(prefetch_count=1)
-    queue_object,consumer_tag = yield channel.basic_consume(queue=basename(__file__),no_ack=False)
+    queue_object,consumer_tag = yield channel.basic_consume(queue=result.method.queue,no_ack=True)
     l = task.LoopingCall(read,queue_object)
     l.start(0.001)
 
@@ -27,11 +33,11 @@ def run(connection):
 def read(queue_object):
     ch,method,properties,body = yield queue_object.get()
     if body:
-        print(body.strip())
-    yield ch.basic_ack(delivery_tag=method.delivery_tag)
+        print('{}\t{}'.format(method.routing_key,body.strip()))
+    #yield ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-credentials = pika.PlainCredentials('nuc',cred['rabbitmq'])
+credentials = pika.PlainCredentials(nodeid,cred['rabbitmq'])
 parameters = pika.ConnectionParameters(credentials=credentials)
 cc = protocol.ClientCreator(reactor,twisted_connection.TwistedProtocolConnection,parameters)
 d = cc.connectTCP('localhost',5672)
