f4580edf817b9f3a5e386cd562745a06b8c49234
Stanley Lio <stanleylio@gmail.com>
2017-10-13 11:34:43 +0000

    limit node-006 plot_range

--- config/poh/node_006.py
+++ config/poh/node_006.py
@@ -3,8 +3,6 @@ name = 'Windbird'
 location = 'First Mākāhā'
 note = 'RM Young 05106 Anemometer'
 
-plot_range = 3*24
-
 
 conf = [
     {
@@ -12,12 +10,14 @@ conf = [
         'dbtype':'DOUBLE NOT NULL',
         'description':'Time of sampling',
         'plot':False,
+        'plot_range':3*24,
         'interval':1,
     },
     {
         'dbtag':'wind_mps',
         'description':'Wind speed',
         'unit':'m/s',
+        'plot_range':3*24,
         'lb':0,
         'interval':1,
     },
@@ -25,6 +25,7 @@ conf = [
         'dbtag':'wind_dir_deg',
         'description':'Wind direction',
         'unit':'Deg',
+        'plot_range':3*24,
         'lb':0,
         'ub':360,
         'interval':1,
