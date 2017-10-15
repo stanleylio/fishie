751493267e9e9e93ef0173cb8fa64e02578c51e9
Stanley Lio <stanleylio@gmail.com>
2017-10-15 09:16:05 +0000

    regular update

--- config/uhm/base_011.py
+++ config/uhm/base_011.py
@@ -5,6 +5,13 @@ note = 'bbb-based'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDC+ZgKHDCHMKOOGw8bcOxAC6Fib5OAtBVgPVNq42g58tTddu8XWLjYVwLlt8YZ6IAMUgnMdRrTLJJK+EEy50nXhSm4Zft2A3VU0YdxQJEp/LwyJJawOI0695oliSMVLCKcrGdZ25A7zR+jU2TdO5EELWqpiqdk6Nwfp3/zKrVDtm6MHhh13iH7kP2vTEsXg79yMLtuGuwAL8tPakVjik1/lC0FwMDOCffE8Rfdy4UCcTvqJwPhkL1cpN6z8UPbBePkJWCt2SKqitZYNk0bITh9ZwPHC78cP3IzcKbSJoFEQbKmmcDEvMMSwYsHmKhknPUKBO9Z4CMI3n0I0ljjWxr nuc@base-011'
 
 
+XBEE_PORT = '/dev/ttyS1'
+XBEE_BAUD = 115200
+INTERVAL = 10
+NGROUP = 1
+XBEELOGDIR = '/var/uhcm/log'
+
+
 conf = [
     {
         'dbtag':'system_clock',
