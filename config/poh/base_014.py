24704ee5168b3cd556af844129399406e8f60ac3
Stanley Lio <stanleylio@gmail.com>
2018-01-24 02:03:09 +0000

    config update

--- config/poh/base_014.py
+++ config/poh/base_014.py
@@ -2,7 +2,7 @@
 name = 'Fūjin (2.4)'
 location = 'Paepae o He\'eia, Kaneohe'
 google_earth_link = 'https://goo.gl/maps/ECEBgo3UEp82'
-note = 'XBee 900HP (cold backup) and 2.4GHz (anemometer controller for node-006). bbb-based, eMMC'
+note = 'XBee 900HP (cold standby) and 2.4GHz (anemometer controller for node-006). bbb-based, eMMC'
 public_key = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUNeHyYu/A3z7pPmXgmR4f6AvCJDtcuSaF1Swf5h/szppdQBrSbXK0L6D6nA+CeGDHmM6OJK0F0JVeq+AHk77c+LZ98ngz0Nd49aagvRJIQJV25PKpyvugsp9MAwYZloiSQuNTObHZlDUhmhoea5cyuZ8xPVi2837X+5kt0ODZuSpqqtGoFiCVuhVfxo6axqsnnjUaMLFGSj0hmYnfKTMxVwl5hyKotarLAioZer2k+aDUm+I1xH3ez/XWo3uoaNx+l4UmDubk5hjRZ0//fl2kIcW6yKAoKl+VyDvBbaOavwQqbS2LIQreBaaOe5qvvMLhvItzboreL2BqJ7R9QpE3 nuc@base-014'
 
 
