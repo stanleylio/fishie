3e56c75d6b25e37263c1c99c4ee696ccf9fa8392
Stanley Lio <stanleylio@gmail.com>
2018-02-09 08:38:46 +0000

    config update

--- config/uhm/node_091.py
+++ config/uhm/node_091.py
@@ -1,7 +1,7 @@
-name = 'Engine Block Temperature'
-location = 'MSB301, UHM'
+name = 'T16'
+location = 'MSB, UHM'
 google_earth_link = '#'
-note = 'Pi 2 with LCD'
+note = 'Tube in a tube in a tube in a tube (TSYS01 stability test)'
 public_key = ''
 
 XBEE_PORT = '/dev/ttyUSB0'
@@ -9,9 +9,6 @@ XBEE_BAUD = 115200
 INTERVAL = 60
 NGROUP = 1
 XBEELOGDIR = '/var/uhcm/log'
-
-subscribeto = ['127.0.0.1:9002']
-log2txt_output_path = '/var/uhcm/log'
 
 
 conf = [
@@ -23,147 +20,131 @@ conf = [
     },
     {
         'dbtag':'tsys0',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH0 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys1',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH0 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys2',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH1 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys3',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH1 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys4',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH2 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys5',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH2 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys6',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH3 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys7',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH3 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys8',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH4 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys9',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH4 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys10',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH5 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys11',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH5 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys12',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH6 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys13',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH6 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys14',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH7 0x76)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
     {
         'dbtag':'tsys15',
-        'dbtype':'DOUBLE',
         'unit':'Deg.C',
         'description':'TSYS01 (CH7 0x77)',
         'interval':60,
-        'lb':-10,
-        'ub':60,
+        'lb':10,
+        'ub':40,
     },
 ]
 
