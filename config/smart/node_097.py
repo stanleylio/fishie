5125888338d8a06a4a396264fb085c01e20e12ae
Stanley Lio <stanleylio@gmail.com>
2018-04-25 11:57:11 -1000

    from us10b to us10c, ticker replaced by idx

--- config/smart/node_097.py
+++ config/smart/node_097.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# successor to us2
 name = 'Water Level'
 location = '(TBD)'
 note = 'Ultrasonic tide gauge (v4.2). One sample per minute. Each sample is the sample mean of the past 60 measurements taken at 1Hz. Telemetry only. No RTC.'
@@ -26,6 +25,13 @@ conf = [
         'description':'1Hz ticker',
         'lb':0,
         'interval':60,
+        'plot':False,
+    },
+    {   # had ticker when running us10b. later changed to us10c and ticker was replaced with idx.
+        'dbtag':'idx',
+        'description':'Sample index',
+        'lb':0,
+        'interval':60,
     },
 ]
 
@@ -34,10 +40,10 @@ if '__main__' == __name__:
     for c in conf:
         print('- - -')
         for k,v in c.items():
-            print(k,':',v)
+            print(k, ':', v)
 
     import sys
     sys.path.append('../..')
     from os.path import basename
     from storage.storage2 import create_table
-    create_table(conf,basename(__file__).split('.')[0].replace('_','-'))
+    create_table(conf, basename(__file__).split('.')[0].replace('_', '-'))
