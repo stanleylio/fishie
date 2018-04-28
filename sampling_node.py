efab214dc6ca68c78b6ae88969af16d244d785a1
Stanley Lio <stanleylio@gmail.com>
2018-04-28 03:21:32 +0000

    .

--- sampling_node.py
+++ sampling_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # logging script for sensor node
 #
@@ -6,35 +6,41 @@
 #
 # Stanley H.I. Lio
 # hlio@hawaii.edu
-# All Rights Reserved. 2017
-import serial,sys,time,traceback,logging,json,pika,socket
-from os.path import join,exists,expanduser
+# All Rights Reserved. 2018
+import serial, sys, time, traceback, logging, json, pika, socket, argparse
+from os.path import join, exists, expanduser
 sys.path.append(expanduser('~'))
-import logging,logging.handlers
+import logging, logging.handlers
 from datetime import datetime
 from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
-#from random import random
 from node.helper import dt2ts
 from node.parse_support import pretty_print
-from node.z import send,get_action
+from node.z import send, get_action
 from node.drivers.indicators import *
-from node.config.config_support import import_node_config
 from cred import cred
 
 
 logging.getLogger('pika').setLevel(logging.WARNING)
 
 
+parser = argparse.ArgumentParser(description='ding')
+parser.add_argument('--NGROUP', default=1, type=int, help='number of samples to take every period')
+parser.add_argument('--INTERVAL', default=60, type=int, help='seconds to wait between group of samples')
+parser.add_argument('--XBEE_PORT', default=None, type=str, help='serial port to XBee')
+parser.add_argument('--XBEE_BAUD', default=115200, type=int, help='XBee baud rate')
+parser.add_argument('--XBEE_LOG_DIR', default=None, type=str, help='where to store XBee traffic overheard')
+
+args = parser.parse_args()
+
 exchange = 'uhcm'
 nodeid = socket.gethostname()
-config = import_node_config()
 
-NGROUP = getattr(config,'NGROUP',1)
-INTERVAL = getattr(config,'INTERVAL',60)
-XBEE_PORT = getattr(config,'XBEE_PORT',None)
-XBEE_BAUD = getattr(config,'XBEE_BAUD',115200)
-XBEE_LOG_DIR = getattr(config,'XBEE_LOG_DIR',None)
+NGROUP = args.NGROUP
+INTERVAL = args.INTERVAL
+XBEE_PORT = args.XBEE_PORT
+XBEE_BAUD = args.XBEE_BAUD
+XBEE_LOG_DIR = args.XBEE_LOG_DIR
 
 
 #'DEBUG,INFO,WARNING,ERROR,CRITICAL'
@@ -58,8 +64,8 @@ if XBEE_PORT is None:
     ser = None
 else:
     assert exists(XBEE_PORT)
-    ser = serial.Serial(xbee_port,xbee_baud,timeout=1)
-    xbeesend = lambda m: send(ser,m)
+    ser = serial.Serial(XBEE_PORT, XBEE_BAUD, timeout=1)
+    xbeesend = lambda m: send(ser, m)
 
 indicators_setup()
 
@@ -71,24 +77,24 @@ xbeesend({'status':'online',
 
 def rabbit_init():
     credentials = pika.PlainCredentials(nodeid,cred['rabbitmq'])
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, '/', credentials))
     channel = connection.channel()
-    channel.exchange_declare(exchange=exchange,exchange_type='topic',durable=True)
-    return connection,channel
+    channel.exchange_declare(exchange=exchange, exchange_type='topic', durable=True)
+    return connection, channel
 
 
 if XBEE_LOG_DIR is None:
     XBEE_LOG_FILE = '/dev/null'
 else:
-    XBEE_LOG_FILE = join(config.XBEELOGDIR,'tsraw.txt')
+    XBEE_LOG_FILE = join(XBEE_LOG_DIR, 'tsraw.txt')
 
 
 # logging all incoming XBee traffic... just because.
-rawf = open(XBEE_LOG_FILE,'a+',1)
+rawf = open(XBEE_LOG_FILE, 'a+', 1)
 def logtsraw(line):
     dt = datetime.utcnow()
     ts = dt2ts(dt)
-    rawf.write('{}\t{}\t{}\n'.format(dt.isoformat(),ts,line.strip()))
+    rawf.write('{}\t{}\t{}\n'.format(dt.isoformat(), ts,line.strip()))
     rawf.flush()
 
 
@@ -156,11 +162,11 @@ def taskSampling():
         payback()
         if debt > 0:
             logger.debug('debt={}'.format(debt))
-            #reactor.callLater(2*random(),taskSampling)
+            #reactor.callLater(2*random(), taskSampling)
         else:
             logger.debug('all debts are paid')
     except pika.exceptions.ConnectionClosed:
-        connection,channel = None,None
+        connection, channel = None, None
         logging.error('connection closed')  # connection to the local exchange closed? wut?
     except:
         logger.error(traceback.format_exc())
@@ -169,7 +175,7 @@ def taskTrigger():
     try:
         if not borrow(NGROUP):
             logging.warning('ran out of credit')
-        #reactor.callLater(0,taskSampling)
+        #reactor.callLater(0, taskSampling)
     except:
         logger.error(traceback.format_exc())
 
@@ -200,7 +206,7 @@ def taskBlink():
 LoopingCall(taskSampling).start(0.1)
 LoopingCall(taskTrigger).start(INTERVAL)
 if ser is not None:
-    LoopingCall(taskSerial).start(0.05,now=False)
+    LoopingCall(taskSerial).start(0.05, now=False)
 LoopingCall(taskBlink).start(1)
 
 logger.info(__name__ + ' is ready')
