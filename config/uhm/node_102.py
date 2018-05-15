2f19b697f40d126e9229e00d06056fe55fe068a7
Stanley Lio <stanleylio@gmail.com>
2018-05-14 17:17:12 -1000

    .

--- config/uhm/node_102.py
+++ config/uhm/node_102.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-name = '"A-truth-universally-acknowledged"'
+name = '"A-Truth-Universally-Acknowledged"'
 location = '(TBD)'
 note = 'Beaglebone-based node measuring oxygen, temperature, conductivity, and pressure'
 
