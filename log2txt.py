d40c373144a1f24bdc5f477826ae27a6eb164446
Stanley Lio <stanleylio@gmail.com>
2018-06-15 13:47:32 -1000

    now recording routing_key

--- log2txt.py
+++ log2txt.py
@@ -5,7 +5,7 @@
 # Stanley H.I. Lio
 # hlio@hawaii.edu
 # University of Hawaii
-# All Rights Reserved, 2017
+# All Rights Reserved. 2018
 import sys, pika, socket, logging, argparse
 from os.path import exists, join, expanduser, basename
 sys.path.append(expanduser('~'))
@@ -32,7 +32,7 @@ logging.basicConfig(level=logging.INFO)
 credentials = pika.PlainCredentials(nodeid, cred['rabbitmq'])
 connection = pika.BlockingConnection(pika.ConnectionParameters(args.brokerip, args.brokerport, '/', credentials))
 channel = connection.channel()
-channel.basic_qos(prefetch_count=100)
+channel.basic_qos(prefetch_count=50)
 channel.exchange_declare(exchange=exchange, exchange_type='topic', durable=True)
 result = channel.queue_declare(queue=basename(__file__),
                                durable=True,
@@ -46,24 +46,24 @@ channel.queue_bind(exchange=exchange,
 
 # product of this script: the raw text file
 config = import_node_config()
-output_path = getattr(config,'log2txt_output_path',None)
+output_path = getattr(config, 'log2txt_output_path', None)
 assert output_path is not None and exists(output_path)
 
 tsraw = open(join(output_path,'tsraw.txt'), 'a', 1)
 
-def callback(ch,method,properties,body):
+def callback(ch, method, properties, body):
     print('= = = = = = = = = =')
     print(body)
 
     dt = datetime.utcnow()
-    tsraw.write('{}\t{:6f}\t{}\n'.format(dt.isoformat(), dt2ts(dt), body.strip()))
+    tsraw.write('{}\t{:6f}\t{}\t{}\n'.format(dt.isoformat(), dt2ts(dt), method.routing_key, body.strip()))
 
     tsraw.flush()
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 logging.info(__file__ + ' is ready')
-channel.basic_consume(callback,queue=queue_name)    # ,no_ack=True
+channel.basic_consume(callback, queue=queue_name)    # ,no_ack=True
 try:
     channel.start_consuming()
 except KeyboardInterrupt:
