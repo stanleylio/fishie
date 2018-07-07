cdcc0f52f49684de7ae6f8675726562cf1a6a99f
Stanley Lio <stanleylio@gmail.com>
2018-07-06 17:13:28 -1000

    config update

--- config/uhm/node_078.py
+++ config/uhm/node_078.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-name = '(Vancouver)'
-location = '(Somewhere in Vancouver, Canada)'
+name = '(TBD)'
+location = '(TBD)'
 note = 'Cellular ultrasonic tide gauge. Each sample is average of 60 measurements taken every second. One transmission every 10 samples. Firmware p5e, hardware v0.2 (with US_EN patch).'
 
 coreid = '320035000f47363334373734'
--- config/uhm/node_079.py
+++ config/uhm/node_079.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-name = '(Vancouver)'
-location = '(Somewhere in Vancouver, Canada)'
-note = 'Cellular ultrasonic tide gauge. One measurement per minute, one transmission every 10 measurements. Firmware p5f, hardware v0.2 (with US_EN patch).'
+name = '(TBD)'
+location = '(TBD)'
+note = 'Cellular ultrasonic tide gauge. Each sample is average of 60 measurements taken every second. One transmission every 10 samples. Firmware p6c, hardware v0.4.'
 
 coreid = '290047000347363334383136'
 
