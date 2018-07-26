8b80f51dd27aa6331e853bf11c836f4f77e1a020
Stanley Lio <stanleylio@gmail.com>
2018-07-26 09:42:52 -1000

    pi cape buzzer

---
+++ drivers/beep.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def beep(on=0.1, off=0.9):
+    pin = 18
+
+    try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(on)
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(off)
+        GPIO.cleanup()
+    except:
+        GPIO.cleanup()
+        raise
+
+
+if '__main__' == __name__:
+    while True:
+        try:
+            beep()
+        except KeyboardInterrupt:
+            break
