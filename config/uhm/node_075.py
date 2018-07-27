475fc0190b5818bc9acd0daed9a3cd7c15e7c175
Stanley Lio <stanleylio@gmail.com>
2018-07-26 21:35:45 -1000

    config update

--- config/uhm/node_075.py
+++ config/uhm/node_075.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 name = 'Maury\'s'
 location = 'Hale o Lono'
-note = 'Cellular ultrasonic tide gauge. Each sample is average of 60 measurements taken every second. One transmission every 10 samples. Firmware p6c, hardware v0.4.'
+note = 'Cellular ultrasonic tide gauge. Each sample is average of 60 measurements taken every second. One transmission every 10 samples. Firmware p6e, hardware v0.3.'
 
 coreid = '230053001951353338363036'
 
