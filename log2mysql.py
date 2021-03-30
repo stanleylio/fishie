1dd23fb83793fb8f9ff938e6a09f226b363270f3
Stanley Lio <stanleylio@gmail.com>
2021-03-30 00:41:04 -1000

    added hostname to queue name

--- log2mysql.py
+++ log2mysql.py
@@ -1,26 +1,30 @@
-# Fetch sensor data from broker to database.
-#
-# So publish/subscribe doesn't revolve around queues (except when used as a work queue).
-# Fanout is done on the exchange level, so each consumer should have its own queue to
-# receive a copy of the same message.
-#
-# Persistence: if the queue is not durable, there's no point in switching over to RabbitMQ
-# from 0mq; if the queue is durable, then it's one more name to remember (monitor). And if
-# TTL is not set, undelivered messages could fill up disk space if consumer is down.
-#
-# TTL: Per-queue or per-message?
-# Policies for the X2mysql, X2stdout, X2txt etc. are different (X2mysql wants everything,
-# X2stdout wants only the fresh ones).
-#
-# TTL=72hr means I have 72hr to detect and mitigate a problem before data loss.
-# Though are they discarded or are they dead-lettered? TODO
-#
-# Stanley H.I. Lio
-# hlio@hawaii.edu
-# University of Hawaii, 2020
+"""Move sensor data from broker to database.
+
+So publish/subscribe doesn't revolve around queues (except when used as
+a work queue). Fanout is done on the exchange level, so each consumer
+should have its own queue to receive a copy of the same message.
+
+On persistence: RabbitMQ is nice because it lets you mess with your
+consumer code while it holds the incoming messages for you. If you let
+the server choose a random queue name for you, then you can't make it
+durable since otherwise you won't be able to find that queue again on
+restart. On the other hand, if you explicitly name the queue and make it
+durable, then you need to remember to manually delete the queue when you
+change the naming scheme, since the old queue would stick around
+accumulating messages and quickly consuming your disk space.
+
+TTL: Per-queue or per-message? Policies for the X2mysql, X2stdout, X2txt
+etc. are different (X2mysql wants everything, X2stdout wants only the
+fresh ones). TTL=72hr means I have 72hr to detect and mitigate a problem
+before data loss.
+
+Stanley H.I. Lio
+hlio@hawaii.edu
+University of Hawaii, 2021
+"""
 import pika, socket, sys, time, math, MySQLdb, logging, argparse, redis, json, random
 from datetime import timedelta
-from os.path import expanduser, basename
+from os.path import expanduser, basename, splitext
 sys.path.append(expanduser('~'))
 from node.parse_support import parse_message, pretty_print
 from node.storage.storage2 import Storage
@@ -48,20 +52,23 @@ def init_rabbit():
     channel.basic_qos(prefetch_count=32)
 
     channel.exchange_declare(exchange=exchange, exchange_type='topic', durable=True)
-    result = channel.queue_declare(queue=basename(__file__),
+    queue_name = "{}_{}".format(nodeid, splitext(basename(__file__))[0])
+    result = channel.queue_declare(queue=queue_name,
                                    durable=True,
                                    arguments={'x-message-ttl':2**31-1}) # ~24 days.
 
-    queue_name = result.method.queue
+    #queue_name = result.method.queue
     tags = ['*.samples', '*.s', '*.debug', '*.d']
     for tag in tags:
         channel.queue_bind(exchange=exchange, queue=queue_name, routing_key=tag)
     channel.basic_consume(queue_name, callback, auto_ack=False)
     return connection, channel
 
+
 store = Storage()
 redis_server = redis.StrictRedis(host='localhost', port=6379, db=0)
 #redis_server.flushall()
+
 
 def callback(ch, method, properties, body):
     global store, redis_server
